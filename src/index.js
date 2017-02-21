d4b321afaa021804e1fc17a77cc1eaf06c794f90
niowa <andres7480311@mail.ru>
2017-02-21 23:15:04 +0300

    additional_1/feature:implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function sum() {
-  // your solution
+module.exports = function sum(...rest) {
+  if (sum.count === undefined) {
+    sum.count = 0;
+    sum.result = 0;
+  }
+
+  if (rest.length === 0) {
+    sum.count = undefined;
+    return sum.result;
+  }
+  var result = rest.reduce(function (sum, current) {
+    return sum + current;
+  })
+  sum.result += result;
+  return sum;
 }
